f035b5ea6274997afc37059a97c3bf959112cabf
V.Nadolinsky <v.nadolinsky.exceedteam@gmail.com>
2020-03-20 14:02:15 +0300

    latest version

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -6,10 +6,9 @@ exports.test = function (req, res) {
 };
 
 
-exports.product_create = function (req, res ,next) {
+exports.task_create = function (req, res ,next) {
     let product = new Product(
         {
-            // _id: req.body._id,
             text: req.body.text,
             completed: req.body.completed
         }
@@ -22,40 +21,58 @@ exports.product_create = function (req, res ,next) {
     })
 };
 
-exports.product_details = function (req, res,next) {
-    Product.findById(req.params.id, function (err, product) {
+exports.task_details = function (req, res,next) {
+    Product.findById(req.params.id, function (err, value) {
         if (err) return next(err);
-        res.send(product);
+        res.send(value);
     })
 };
 
-exports.product_all = function (req , res) {
-    Product.find({}).then(function (product) {
-    res.send(product);
+exports.task_all = function (req , res) {
+    Product.find({}).then(function (value) {
+    res.send(value);
     });
    }
 
-exports.product_update = function (req, res,next) {
+// exports.product_update = function (req, res,next) {
+//     // let completed; 
+//     // console.log(req.params.completed)
+//     // if(req.body.complited) {
+//     //     completed = false
+//     // }else {
+//     //     completed = true
+//     // }
+//     Product.findByIdAndUpdate(req.params.id, /* {$bit: {completed: {xor: 1 }}},  */{"completed": req.params.completed? false : true}, {new: true},function (err, value) {
+//         if (err) return next(err);
+//         res.send(value);
+//     });
+// };
+
+
+exports.task_update = function (req, res,next) {
     Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
         if (err) return next(err);
         res.send('Product udpated.');
     });
 };
 
-exports.product_update_all = function (req, res,next) {
-    // Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
-    //     if (err) return next(err);
-    //     res.send('Product udpated.');
-    // });
+exports.task_update_all = function (req, res,next) {
     Product.updateMany({}, {"$set":{"completed": req.body.completed}} , (err, value) => {
         if(err) return next(err)
         res.send(value)
     });
 };
 
-exports.product_delete = function (req, res,next) {
-    Product.findByIdAndRemove(req.params.id, function (err) {
+exports.task_delete = function (req, res,next) {
+    Product.findByIdAndRemove(req.params.id, function (err,value) {
         if (err) return next(err);
-        res.send('Deleted successfully!');
+        res.send(value);
     })
 };
+
+exports.task_delete_all_completed = function (req, res , next) {
+    Product.remove( {"completed" : true}, (err,value) => {
+        if(err) return next(err)
+        res.send(value)
+    })
+}
--- routes/product.route.js
+++ routes/product.route.js
@@ -6,11 +6,12 @@ const product_controller = require('../controllers/product.controller');
 
 router.get('/test', product_controller.test);
 
-router.get('/all',product_controller.product_all)
+router.get('/all',product_controller.task_all)
 
-router.get('/:id',product_controller.product_details);
-router.post('/create', product_controller.product_create);
-router.put('/:id/update', product_controller.product_update);
-router.put('/update', product_controller.product_update_all);
-router.delete('/:id/delete', product_controller.product_delete);
+router.get('/:id',product_controller.task_details);
+router.post('/create', product_controller.task_create);
+router.put('/:id/update', product_controller.task_update);
+router.put('/update', product_controller.task_update_all);
+router.delete('/:id/delete', product_controller.task_delete);
+router.delete('/deletecompl', product_controller.task_delete_all_completed) 
 module.exports = router;
